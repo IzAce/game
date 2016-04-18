bcc42165174237dec5a8c2ebd46f43b355b7a64c
Dan4o <danail.julian@gmail.com>
2016-04-18 23:30:49 +0300

    First commit

--- core/src/com/xda/game/Game.java
+++ core/src/com/xda/game/Game.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -23,5 +23,19 @@ public class Game extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void	resize(int width, int height){	}
+
+	@Override
+	public void pause(){}
+
+	@Override
+	public void resume(){}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
 	}
 }
--- desktop/src/com/xda/game/desktop/DesktopLauncher.java
+++ desktop/src/com/xda/game/desktop/DesktopLauncher.java
@@ -6,7 +6,11 @@ import com.xda.game.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Game [In-Dev Build  0.1]";
+		cfg.height = 800;
+		cfg.width = 600;
+		cfg.fullscreen = false;
+		new LwjglApplication(new Game(), cfg);
 	}
 }
