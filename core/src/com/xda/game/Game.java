5fcb28063823c3415ef43510ac0cdd6a51951474
alex4o <mrabonin@gmail.com>
2016-04-19 21:19:30 +0300

    In the begining

--- core/src/com/xda/game/Game.java
+++ core/src/com/xda/game/Game.java
@@ -2,7 +2,9 @@ package com.xda.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.graphics.GL20;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -29,10 +31,14 @@ public class Game extends ApplicationAdapter {
 	public void	resize(int width, int height){	}
 
 	@Override
-	public void pause(){}
+	public void pause(){
+
+	}
 
 	@Override
-	public void resume(){}
+	public void resume(){
+
+	}
 
 	@Override
 	public void dispose () {
